55aa9a45ead2e1dc0567c61b1e748fd2fac0db5b
Jim Fung <jimtheengineer@gmail.com>
2018-09-14 12:42:03 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,24 +4,27 @@ xcode-select --install
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# install my dotfiles
+sh ~/.dotfiles/install
+
+brew bundle install --global
+
+pip3 install neovim --upgrade
+
 mkdir ~/Dropbox
 ln -s ~/Dropbox ~/personal
 mkdir ~/Google\ Drive
 ln -s ~/Google\ Drive ~/work
 mkdir ~/source
 
-# install my dotfiles
-sh ~/.dotfiles/install
-
-# install applications
-brew bundle install --global
-pip3 install neovim --upgrade
+# macOS settings
+sh ./scripts/machine_setup/macOS.sh
 
 # Install the Solarized Dark theme for iTerm
 open "${HOME}/init/Solarized Dark.itermcolors"
 
-# macOS settings
-sh ./scripts/machine_setup/macOS.sh
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
